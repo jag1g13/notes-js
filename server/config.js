3380f13a61fbc9706365c8d66fdd9d052e5389c0
James Graham <j.graham@soton.ac.uk>
2020-07-17 21:52:23 +0100

    fix: Update server port env var
    Resolves conflict between front and backend

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 
 export const db_url = process.env.DB_URL
 
-export const port = process.env.PORT
+export const port = process.env.SERVER_PORT
 
 export const notes_repo_dir = process.env.NOTES_REPO_DIR
 
