43c486dd4c12998d35bea713f5def145e7584b37
James Graham <j.graham@soton.ac.uk>
2020-07-23 21:55:14 +0100

    style: Fix StandardJS complaints in test code

--- src/components/notes.test.js
+++ src/components/notes.test.js
@@ -1,20 +1,22 @@
+/* eslint-env jest */
+
 import React from 'react'
 import { render } from '@testing-library/react'
 
 import { NoteDetailComponent } from './notes.js'
 
 test('displays default message in empty state', () => {
-    const { getByText } = render(<NoteDetailComponent />)
-    const heading = getByText(/No Content/i)
-    expect(heading).toBeInTheDocument()
+  const { getByText } = render(<NoteDetailComponent />)
+  const heading = getByText('No Content')
+  expect(heading).toBeInTheDocument()
 })
 
 test('displays note content', () => {
-    const note = {
-        content: '# TEST HEADING'
-    }
+  const note = {
+    content: '# TEST HEADING'
+  }
 
-    const { getByText } = render(<NoteDetailComponent note={note} />)
-    const heading = getByText(/TEST HEADING/i)
-    expect(heading).toBeInTheDocument()
+  const { getByText } = render(<NoteDetailComponent note={note} />)
+  const heading = getByText('TEST HEADING')
+  expect(heading).toBeInTheDocument()
 })
