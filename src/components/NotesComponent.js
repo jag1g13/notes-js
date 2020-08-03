676cae3d41e27e37a30dac97ad5fce8abb5686e9
James Graham <j.graham@soton.ac.uk>
2020-08-03 19:55:06 +0100

    style: Fix StandardJS complaints in NotesComponent

--- src/components/NotesComponent.js
+++ src/components/NotesComponent.js
@@ -1,3 +1,5 @@
+/* eslint-env browser */
+
 import React from 'react'
 
 import ProjectChartComponent from './ProjectChartComponent.js'
@@ -8,75 +10,78 @@ import NoteListComponent from './NoteListComponent.js'
  * Component providing detail and list views of notes fetched from API.
  */
 export class NotesComponent extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            notes: [],
-            selected: 0
-        }
-
-        this.fetch_notes = this.fetch_notes.bind(this)
-        this.select_note = this.select_note.bind(this)
+  constructor () {
+    super()
+    this.state = {
+      notes: [],
+      selected: 0
     }
 
-    componentDidMount() {
-        this.fetch_notes()
+    this.fetchNotes = this.fetchNotes.bind(this)
+    this.selectNote = this.selectNote.bind(this)
+  }
+
+  componentDidMount () {
+    this.fetchNotes()
+  }
+
+  fetchNotes () {
+    fetch('/api/notes')
+      .then(res => res.json())
+      .then(notes => this.setState({ notes: notes }))
+      .catch(exc => console.log(exc))
+  }
+
+  selectNote (noteId) {
+    this.setState({ selected: noteId })
+  }
+
+  render () {
+    let projects = new Set()
+
+    for (const note of this.state.notes) {
+      for (const project in note.metadata.projects) {
+        projects.add(project)
+      }
     }
 
-    fetch_notes() {
-        fetch('/api/notes')
-            .then(res => res.json())
-            .then(notes => this.setState({ notes: notes }))
-            .catch(exc => console.log(exc))
-    }
+    projects = Array.from(projects).sort()
 
-    select_note(note_id) {
-        this.setState({ selected: note_id })
-    }
+    const datasets = projects.map(project => ({
+      label: project,
+      data: this.state.notes.map(note => {
+        return note.metadata.projects[project] || 0
+      }),
 
-    render() {
-        let projects = new Set()
+      // Add border to selected column
+      borderWidth: context => this.state.selected === context.dataIndex ? 8 : 0,
+      borderSkipped: false,
+      borderColor: 'rgba(0, 0, 0, 0.2)'
 
-        for (const note of this.state.notes) {
-            for (const project in note.metadata.projects) {
-                projects.add(project)
-            }
-        }
+    }))
 
-        projects = Array.from(projects).sort()
+    return (
+      <div className='columns'>
+        <div className='column is-three-quarters'>
+          <ProjectChartComponent
+            data={{
+              labels: this.state.notes.map(note => note.date),
+              datasets: datasets
+            }} select_note={this.selectNote}
+          />
 
-        const datasets = projects.map(project => ({
-            label: project,
-            data: this.state.notes.map(note => {
-                return note.metadata.projects[project] || 0
-            }),
+          <NoteDetailComponent note={this.state.notes[this.state.selected]} />
+        </div>
 
-            // Add border to selected column
-            borderWidth: context => this.state.selected === context.dataIndex ? 8 : 0,
-            borderSkipped: false,
-            borderColor: 'rgba(0, 0, 0, 0.2)'
-
-        }))
-
-        return (
-            <div className="columns">
-                <div className="column is-three-quarters">
-                    <ProjectChartComponent data={{
-                        labels: this.state.notes.map(note => note.date),
-                        datasets: datasets
-                    }} select_note={this.select_note} />
-
-                    <NoteDetailComponent note={this.state.notes[this.state.selected]} />
-                </div>
-
-                <div className="column is-one-quarter">
-                    <NoteListComponent
-                        notes={this.state.notes}
-                        handleRefresh={this.fetch_notes}
-                        select_note={this.select_note}
-                        selected_note={this.state.selected} />
-                </div>
-            </div>
-        )
-    }
+        <div className='column is-one-quarter'>
+          <NoteListComponent
+            notes={this.state.notes}
+            handleRefresh={this.fetchNotes}
+            select_note={this.selectNote}
+            selected_note={this.state.selected}
+          />
+        </div>
+      </div>
+    )
+  }
 }
